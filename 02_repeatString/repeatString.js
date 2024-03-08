ee2e1a7791cada4699d36087f3e73306d53b6408
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-03-08 09:32:56 -0500

    Tested and Completed the 02_repeatString Exercise
    Tested, Completed, and checked the results of the
    exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    const string = word;
+    const num = number;
+    let newString = '';
+    if (num>=0){
+        for (i = 0; i<num; i++){
+            newString += string;
+        }
+    } else {
+        newString = "ERROR";
+    }
+    
+    return newString;
 
 };
 
