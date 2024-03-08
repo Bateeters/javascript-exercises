ff4f2ca22a5647b28d140b833ca38e2d556dcfb7
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-03-08 10:02:55 -0500

    Finished and Tested 03_reverseString Exercise
    Wrote and Tested code to satisfy the tests of
    the 03_reverseString exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const word = string;
+    let reversedWord = '';
+    
+    let wordArray = word.split('');
 
+    for (i = 0; i<word.length; i++){
+        lastLetter = wordArray.pop();
+        reversedWord += lastLetter;
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
