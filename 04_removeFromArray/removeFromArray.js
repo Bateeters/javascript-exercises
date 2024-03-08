3bba8e5313dd6f84fc0f77f8f58ecee3d13cdfce
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-03-08 11:11:29 -0500

    Finished the 04_removeFromArray Exercise
    Coded, tested, and reviewed my solution for the
    04_removeFromArray exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...theArg) {
 
+    const newArray = [];
+
+    // Iterate through the given array, 
+    // element holds the value of the current array item
+    myArray.forEach((element) => {
+
+        // If "theArg" rest param does not have the current element,
+        if (!theArg.includes(element)){
+
+            // Add the current element to newArray
+            newArray.push(element);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
