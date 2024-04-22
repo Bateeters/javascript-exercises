1124e242975d498d1cfc9e7b74ac88b9c8a56590
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-21 20:12:19 -0400

    Updated calculator.js to include sum and subtract
    Sum and Subtract functions are now functional and
    the test cases have been successfully completed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,13 +3,18 @@ const add = function(num1, num2) {
   return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
-};
+const sum = function(numbers) {
+  const sumOfAllNums = numbers.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return sumOfAllNums;
+
+  };
 
 const multiply = function() {
 
