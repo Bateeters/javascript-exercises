e2da6d6125af40ed6452576c8c1633252dbf9efd
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 10:57:12 -0400

    Added comments for future reference

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,34 +1,29 @@
 const fibonacci = function(number) {
 
     const fibArr = [1]; // initial array starting with 1
-    let input = Number(number);
-    console.log(typeof input);
+    let input = Number(number); // change user input to a number
 
     if(input > 0){ // check if given number is greater than 1
         let i = 1;
         let start = 1;
 
         while(fibArr.length < input-1){
-            fibArr[i]=start;
-            // console.log(fibArr);
+            fibArr[i]=start; // add the next number to the fibArr
 
-            let lastNum = Number(fibArr.slice(-1));
-            fibArr.pop();
-            let secondLastNum = Number(fibArr.slice(-1));
+            let lastNum = Number(fibArr.slice(-1)); // get the last number of fibArr
+            fibArr.pop(); // remove last number of fibArr
+            let secondLastNum = Number(fibArr.slice(-1)); // get the second last number of fibArr
             
-            start = lastNum+secondLastNum;
-            fibArr.push(lastNum);
-            // console.log(start, fibArr);
+            start = lastNum+secondLastNum; // add the last 2 numbers together for next loop
+            fibArr.push(lastNum); // add the removed last number back to end of fibArr
             i++;
         }
 
-        let fib1 = Number(fibArr.slice(-1));
-        fibArr.pop();
+        let fib1 = Number(fibArr.slice(-1)); // Get last number in array
+        fibArr.pop(); // remove last number in array
 
-        let fib2 = Number(fibArr.slice(-1));
-
-        // console.log(fib1+fib2);
-        return fib1+fib2;
+        let fib2 = Number(fibArr.slice(-1)); // Get the NEW last number in array
+        return fib1+fib2; // add and return the sum of the last two numbers
     } else if (input<0){
         return "OOPS";
     } else {
