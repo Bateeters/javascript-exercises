8f45349c8d13cf2e6f2b9ba02990da51676139c0
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 09:51:26 -0400

    Updated palindromes function
    function now works with punctuation, spaces, and upper case letters.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,22 @@
 const palindromes = function (input) {
     const string = input; // assigns input to string
-    const reversed = string.split('').reverse().join(''); // splits "string" into an array, reverses it, and rejoins it to a string
-    return string === reversed; // check to see if both strings are the same
+    const accepted = 'abcdefghijklmnopqrstuvwxyz0123456789'; // accepted characters
+
+    // filter out any characters not in "accepted"
+    const filteredString = string
+        .toLowerCase() // turn entire string to lower case
+        .split('') // turn string into array to use .filter()
+        .filter((char) => accepted.includes(char)) // removes anything not in "accepted"
+        .join(''); // join array back to a string
+
+    // splits "filteredString" into an array, reverses it, and rejoins it to a string
+    const reversed = filteredString
+        .split('')
+        .reverse()
+        .join('');
+
+    console.log(filteredString, reversed);
+    return filteredString === reversed; // check to see if both strings are the same
 };
 
 // Do not edit below this line
