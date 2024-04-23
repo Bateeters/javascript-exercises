7bbe43abf1fd09b60074a0047a541c043a3cc0b8
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 09:38:46 -0400

    Added notes, comments, to code for reference

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,7 @@
 const palindromes = function (input) {
-    const string = input;
-    const reversed = string.split('').reverse().join('');
-    console.log(string, reversed);
-    return string === reversed;
+    const string = input; // assigns input to string
+    const reversed = string.split('').reverse().join(''); // splits "string" into an array, reverses it, and rejoins it to a string
+    return string === reversed; // check to see if both strings are the same
 };
 
 // Do not edit below this line
