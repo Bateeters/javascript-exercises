9648e2143b02b8e909e3cf7dde63427563f0dec2
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 09:36:19 -0400

    Added code to palindromes function
    started coding and passed first conditional test
    for 09_palindromes exercise. Works with single words.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const string = input;
+    const reversed = string.split('').reverse().join('');
+    console.log(string, reversed);
+    return string === reversed;
 };
 
 // Do not edit below this line
