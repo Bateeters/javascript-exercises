6f58f96f58fce8ee3536295a978c521f1e8e8ad8
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 11:15:57 -0400

    Completed 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    // use .map() to go through the given array
+    // "obj" is used to name each object in the array. 
+    // obj.title refers to the object's title since "title" was a predefined object key
+    let titles = array.map((obj)=> obj.title);
+
+    return titles;
 };
 
 // Do not edit below this line
